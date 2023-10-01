5d789ffe43befa8f7642f6ed7656277e3e9b02bd
Joshua Hawatta Azevedo <99093820+JoshuaHawatta@users.noreply.github.com>
2023-10-01 19:05:41 -0300

    Update company.repository.ts

--- src/app/company/repositories/company.repository.ts
+++ src/app/company/repositories/company.repository.ts
@@ -11,7 +11,7 @@ class CompanyRepository {
   ) {}
 
   public async ensureCompanyExists(): Promise<Company> {
-    const company = await this.company.findOne({ name: 'Workmize' });
+    const company = await this.company.findOne({ name: 'Empresa Fake' });
 
     if (company) return company;
 
